895ced2843a28146e30d2ce5217b84ee50fe5403
lgr0113@126.com <l5854531>
2020-03-27 00:59:38 +0800

    system 添加mysql

--- springcloud/system/src/main/java/com/system/SystemApplication.java
+++ springcloud/system/src/main/java/com/system/SystemApplication.java
@@ -1,30 +1,17 @@
 package com.system;
-
-import org.springframework.beans.factory.annotation.Value;
+import tk.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-@RestController
 @EnableEurekaClient
+@MapperScan(basePackages = "com.system.dao")
 public class SystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class, args);
-    }
-
-
-    @Value("${server.port}")
-    String port;
-
-    @RequestMapping("/hi")
-    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        return "hi " + name + " ,i am from port:" + port;
     }
 
 }
